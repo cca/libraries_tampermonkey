613c74141546a398a8595995d56832c47b091c72
phette23 <phette23@gmail.com>
2019-09-05 13:40:46 -0700

    koha catalog - run on all pages
    makes views show up even when you're not on the "normal" view pages

--- koha-catalog.js
+++ koha-catalog.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Koha Catalog for Staff
 // @namespace    https://libraries.cca.edu
-// @version      1.0
+// @version      1.1
 // @description  handy links & functions for staff use of Koha OPAC
 // @author       @phette23
-// @match        https://library.cca.edu/cgi-bin/koha/opac-detail.pl?*
+// @match        https://library.cca.edu/cgi-bin/koha/*
 // @grant        none
 // @require      http://code.jquery.com/jquery-latest.js
 // ==/UserScript==
@@ -12,18 +12,17 @@
 (function() {
     console.log('Running Koha Catalog for Staff script.')
 
-    // show all the "views", normal, marc, ISBD
-    $('#views').show()
+    // show all the "views": normal, marc, ISBD
+    var views = $('#views').show()
 
     // insert link to staff side of Koha in record detail view
     var id = location.search.match(/\?biblionumber=([0-9]+)/)[1]
-    // we don't have access to content page's jQuery so we use vanilla JS
-    var title = encodeURIComponent(document.querySelector('h1.title').innerText)
+    var title = encodeURIComponent($('h1.title').text())
 
     var staff_url = 'https://library-staff.cca.edu/cgi-bin/koha/catalogue/detail.pl?biblionumber=' + id
     var summon_url = 'https://cca.summon.serialssolutions.com/#!/search?l=en&q=' + title
 
-    var views = document.querySelector('#views')
-    views.innerHTML += `<span class="view"><a id="staffView" href="${staff_url}" style="background-position:-10px -56px;">Staff</a></span>`
-    views.innerHTML += `<span class="view"><a id="summonSearch" href="${summon_url}" style="background-position:-10px -56px;">Summon</a></span>`
+    views.append(`<span class="view"><a id="staffView" href="${staff_url}" style="background-position:-10px -56px;">Staff</a></span>`)
+    // title is null on ISBD view
+    if (title) views.append(`<span class="view"><a id="summonSearch" href="${summon_url}" style="background-position:-10px -56px;">Summon</a></span>`)
 })();
