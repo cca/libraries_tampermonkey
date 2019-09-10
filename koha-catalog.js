fffedee289abd57797bd41db9b86e32f6358fa92
phette23 <phette23@gmail.com>
2019-09-10 12:09:21 -0700

    koha - show print, save record actions

--- koha-catalog.js
+++ koha-catalog.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Koha Catalog for Staff
 // @namespace    https://libraries.cca.edu
-// @version      1.1
+// @version      1.1.1
 // @description  handy links & functions for staff use of Koha OPAC
 // @author       @phette23
 // @match        https://library.cca.edu/cgi-bin/koha/*
@@ -25,4 +25,8 @@
     views.append(`<span class="view"><a id="staffView" href="${staff_url}" style="background-position:-10px -56px;">Staff</a></span>`)
     // title is null on ISBD view
     if (title) views.append(`<span class="view"><a id="summonSearch" href="${summon_url}" style="background-position:-10px -56px;">Summon</a></span>`)
+
+    // show "print record" & "export" actions, these are hidden in our OpacUserJS
+    $('.print-large').parent('li').show()
+    $('#export').parent('li').show()
 })();
