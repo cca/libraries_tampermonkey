4bcbe9b0ad49c8fb61bb1520df49a655cb52355f
phette23 <phette23@gmail.com>
2019-11-04 13:05:06 -0800

    bump summon version just to test download/updateURL

--- summon.js
+++ summon.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Summon Mods
 // @namespace    https://libraries.cca.edu
-// @version      1.1.1
+// @version      1.1.2
 // @description  helpful modifications for the Summon discovery layer
 // @author       @phette23
 // @match        https://cca.summon.serialssolutions.com/*
