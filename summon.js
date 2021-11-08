39c28d2a86e184556b6d6d038b461abf8fb279f4
Eric Phetteplace <phette23@gmail.com>
2021-11-08 10:37:37 -0800

    summon - switch link to new ProQuest support center

--- summon.js
+++ summon.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Summon Mods
 // @namespace    https://libraries.cca.edu
-// @version      1.2.3
+// @version      1.2.4
 // @description  helpful modifications for the Summon discovery layer
 // @author       @phette23
 // @match        https://cca.summon.serialssolutions.com/*
@@ -32,10 +32,10 @@
     // format metadata for use in Ex Libris support tickets
     window.report = () => {
         // support ticket text, to be copied to clipboard later
-        var text = 'The link to this article\n\n'
+        let text = 'The link to this article\n\n'
         // angular doc with metadata details
-        var doc = angular.element('.documentSummary').scope().document
-        var authors = doc.authors.reduce((text, author, idx, arr) => {
+        let doc = angular.element('.documentSummary').scope().document
+        let authors = doc.authors.reduce((text, author, idx, arr) => {
             text += author.fullname
             // it's _not_ the last author in the list
             if (arr.length - idx != 1) {
@@ -51,10 +51,9 @@
 
         // citation
         text += `"${doc.full_title}" by ${authors}. _${doc.publication_title}_, ${doc.publication_date}.\n\n`
-
         text += `fails to resolve correctly.\n\nSummon bookmark:\n${'https://cca.summon.serialssolutions.com/#!/search?bookMark=' + doc.bookmark}\n\nOpenURL:\n${doc.open_url}`
 
         prompt("Copy to clipboard with CMD+C", text)
-        open("https://support.exlibrisgroup.com", "_blank")
+        open("https://support.proquest.com/s/login", "_blank")
     }
 })();
