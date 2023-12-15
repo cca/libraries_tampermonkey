32d01156fa2890415adcf6abc24b16b4bd0cbad3
phette23 <phette23@gmail.com>
2023-12-15 11:24:31 -0800

    refstats: remove jQuery dependency, make work with require-trusted-types CSP

--- refstats.js
+++ refstats.js
@@ -1,29 +1,34 @@
 // ==UserScript==
 // @name         Refstats Interaction Templates
 // @namespace    https://libraries.cca.edu
-// @version      1.6.0
+// @version      2.0.0
 // @description  fill out common refstats interactions in a single click
 // @author       @phette23
 // @match        https://docs.google.com/a/cca.edu/forms/d/e/*
 // @match        https://docs.google.com/forms/u/*/d/e/*
 // @match        https://docs.google.com/forms/d/e/*
 // @grant        none
-// @require      https://code.jquery.com/jquery-3.7.0.slim.min.js
 // @updateURL    https://raw.githubusercontent.com/cca/libraries_tampermonkey/main/refstats.js
 // @downloadURL  https://raw.githubusercontent.com/cca/libraries_tampermonkey/main/refstats.js
 // ==/UserScript==
-/*global $*/
+/*global trustedTypes*/
 (function() {
     console.log('Running Refstats Interaction Templates script.')
+    const d = document
 
-    function fillOutForm (fields) {
+    function fillOutForm(fields) {
         for (let field of fields.split(',')) {
-            $(`label:contains(${field})`).click()
+            d.querySelectorAll('label').forEach(label => {
+                if (label.textContent.includes(field)) {
+                    label.click()
+                }
+            })
         }
     }
 
     // only run on ref stats form
-    if ($('[role="heading"]').first().text().trim().toLowerCase() == 'reference statistics form') {
+    const heading = d.querySelector('[role="heading"]').textContent.trim().toLowerCase()
+    if (heading == 'reference statistics form') {
         let defaults = [
             { title: "Fac Portal Email", fields: "Online,Technical/Computing,Email,Faculty,Portal" },
             { title: "Moo Tech Chat", fields: "Online,Technical/Computing,Chat,Faculty,Moodle" },
@@ -33,14 +38,26 @@
         defaults.forEach((tpl) => {
             html += `<div style="cursor:pointer; color:blue" role="button" class="js-tampermonkey" data-fields="${tpl.fields}">${tpl.title}</div>&nbsp;|&nbsp;`
         })
-        $('.freebirdFormviewerViewHeaderTitleRow, [role="heading"]').first().after(html)
-        $('.js-tampermonkey').click((event) => {
-            event.preventDefault()
-            let fields = $(event.target).data('fields')
-            if (fields) fillOutForm(fields)
+
+        // work around Google Forms CSP require-trusted-types-for 'script'
+        // see: https://developer.mozilla.org/en-US/docs/Web/API/TrustedHTML
+        if (window.trustedTypes) {
+            const trusted = trustedTypes.createPolicy("policy", {
+                createHTML: (string) => string
+            })
+            html = trusted.createHTML(html)
+        }
+
+        d.querySelector('.freebirdFormviewerViewHeaderTitleRow, [role="heading"]').insertAdjacentHTML('afterend', html)
+        d.querySelectorAll('.js-tampermonkey').forEach(element => {
+            element.addEventListener('click', (event) => {
+                event.preventDefault()
+                let fields = event.target.dataset.fields;
+                if (fields) fillOutForm(fields)
+            })
         })
         // auto-select email address
-        let email = $('div[data-user-email-address]').find('div[role="checkbox"]')
-        if (email.attr('aria-checked') === 'false') email.click()
+        let email = d.querySelector('div[data-user-email-address]').querySelector('div[role="checkbox"]')
+        if (email.ariaChecked === 'false') email.click()
     }
 })();
