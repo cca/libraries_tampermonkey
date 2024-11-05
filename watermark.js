4612b374a74e32a463ac5178036b8cbbf57bf1f9
phette23 <phette23@gmail.com>
2024-11-05 14:24:29 -0800

    fix: watermark script runs now

--- watermark.js
+++ watermark.js
@@ -1,17 +1,18 @@
 // ==UserScript==
 // @name         Watermark Course Evaluations
 // @namespace    https://libraries.cca.edu
-// @version      1.0.0
+// @version      1.3.0
 // @description  un-select all Extension courses in Watermark course evaluations
 // @author       @phette23
-// @match        https://cca.evaluationkit.com/Project/Moodle.aspx
+// @match        https://cca.evaluationkit.com/Project/Moodle.aspx*
 // @grant        none
 // @updateURL    https://raw.githubusercontent.com/cca/libraries_tampermonkey/main/watermark.js
 // @downloadURL  https://raw.githubusercontent.com/cca/libraries_tampermonkey/main/watermark.js
 // ==/UserScript==
 /*global $*/
-(function () {
-    $('ul.nav.navbar-nav').eq(0).append('<li><a href="#" id="ccarmextension">Unselect Extension Courses</a></li>')
+setTimeout(() => {
+    const id = "ccarmextension"
+    if (!$(`#${id}`).length) $('ul.nav.navbar-nav').eq(0).append(`<li><a href="#" id="${id}">Unselect Extension Courses</a></li>`)
     $('#ccarmextension').on('click', (event) => {
         event.preventDefault()
         $('#ctl00_WorkArea__Panel1_gridView tr').each((idx, el) => {
@@ -20,4 +21,4 @@
             if (courseCode.startsWith('EX')) tds.eq(0).find('input')[0].checked = false
         })
     })
-})
+}, 1000)
