616e9849f6a3e12a0ec298c6b29602e55d31a30d
phette23 <phette23@gmail.com>
2025-05-30 14:41:00 -0700

    feat: link to edit Koha record
    also refactor the Koha catalog script a bit
    use const instead of var
    if condition before adding Koha staff side links
    reorganize to better group related code

--- koha-catalog.js
+++ koha-catalog.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Koha Catalog for Staff
 // @namespace    https://libraries.cca.edu
-// @version      1.2.1
+// @version      1.3.0
 // @description  handy links & functions for staff use of Koha OPAC
 // @author       @phette23
 // @match        https://library.cca.edu/cgi-bin/koha/*
@@ -16,20 +16,23 @@ jQuery(document).ready(function($){
     console.log('Running Koha Catalog for Staff script.')
 
     // show all the "views": normal, marc, ISBD
-    var views = $('#views').show()
+    const views = $('#views').show()
     // show total results, informative for us & useless for patrons
     $('#numresults').show()
 
-    // insert link to staff side of Koha in record detail view
-    var id = new URLSearchParams(location.search).get('biblionumber')
-    var title = encodeURIComponent($('h1.title').text())
+    // insert links to edit & view record on staff side
+    const id = new URLSearchParams(location.search).get('biblionumber')
+    if (id) {
+        views.append(`<span class="view"><a id="staffView" href="https://library-staff.cca.edu/cgi-bin/koha/cataloguing/editor.pl#catalog/${id}" style="background-position:-10px -56px;">Edit MARC</a></span>`)
+        views.append(`<span class="view"><a id="staffView" href="https://library-staff.cca.edu/cgi-bin/koha/catalogue/detail.pl?biblionumber=${id}" style="background-position:-10px -56px;">Staff</a></span>`)
+    }
 
-    var staff_url = 'https://library-staff.cca.edu/cgi-bin/koha/catalogue/detail.pl?biblionumber=' + id
-    var summon_url = 'https://cca.summon.serialssolutions.com/#!/search?fvf=SourceType,Library%20Catalog&q=' + title
-
-    views.append(`<span class="view"><a id="staffView" href="${staff_url}" style="background-position:-10px -56px;">Staff</a></span>`)
-    // title is null on ISBD view
-    if (title) views.append(`<span class="view"><a id="summonSearch" href="${summon_url}" style="background-position:-10px -56px;">Summon</a></span>`)
+    // link to Summon search, title is null in ISBD view
+    const title = encodeURIComponent($('h1.title').text())
+    if (title) {
+        const summon_url = 'https://cca.summon.serialssolutions.com/#!/search?fvf=SourceType,Library%20Catalog&q=' + title
+        views.append(`<span class="view"><a id="summonSearch" href="${summon_url}" style="background-position:-10px -56px;">Summon</a></span>`)
+    }
 
     // show "print record" & "export" actions, these are hidden in our OpacUserJS
     $('.print-large').parent('li').show()
